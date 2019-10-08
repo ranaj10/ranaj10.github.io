dc2c557fb03b55b627e7cc4ed1e7e2b576de73f2
ranaj10 <45240957+ranaj10@users.noreply.github.com>
2019-10-08 16:11:13 +0530

    Add files via upload

---
+++ sanjhicycle.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */
+
+public class sanjhicycle {
+    
+    public static void main(String args[])
+    {
+         home_page hp=new home_page();
+        hp.setVisible(true);
+    }
+    
+}
